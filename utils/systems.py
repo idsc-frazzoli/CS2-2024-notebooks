7d06fc28ee9f9be0a08d84c5aca9b6c5fb6802ec
Timm Grigat <thgrigat@gmail.com>
2024-04-02 01:43:50 +0200

    Added additional functionallity to the library

--- utils/systems.py
+++ utils/systems.py
@@ -1,4 +1,7 @@
-"""This module contains functions to access the system library."""
+"""This module contains functions to access the system library.
+If you want to use this module in your jupyter notebook, you need to
+install the pyyaml package. You can do this by running the following
+command in your terminal: %pip install pyyaml."""
 
 import yaml
 import control as ct
@@ -7,12 +10,12 @@ from pathlib import Path
 system_library = Path(__file__).parent / "systems" / "systems.yaml"
 
 
-def library_tf(system: int) -> tuple[ct.TransferFunction, str]:
-    """Returns the a tuple containing of the transfer function and the
+def library_tf_SISO(system: int) -> tuple[ct.TransferFunction, str]:
+    """Returns the a tuple containing the transfer function and the
     name of the system."""
     with open(system_library, "r") as f:
         try:
-            systems = yaml.safe_load(f)
+            systems = yaml.safe_load(f)["Num/Den SISO"]
             if system > len(systems):
                 raise ValueError(f"System {system} not found in systems.yaml")
             tf = ct.TransferFunction(systems[system]["num"], systems[system]["den"])
@@ -20,3 +23,33 @@ def library_tf(system: int) -> tuple[ct.TransferFunction, str]:
 
         except yaml.YAMLError as exc:
             raise Exception(exc)
+
+def library_ss_SISO(system: int) -> tuple[ct.StateSpace, str]:
+    """Returns the a tuple containing the state space representation and the
+    name of the system."""
+    with open(system_library, "r") as f:
+        try:
+            systems = yaml.safe_load(f)["State Space SISO"]
+            if system > len(systems):
+                raise ValueError(f"System {system} not found in systems.yaml")
+            ss = ct.StateSpace(systems[system]["A"], systems[system]["B"],
+                               systems[system]["C"], systems[system]["D"])
+            return ss, systems[system]["name"]
+
+        except yaml.YAMLError as exc:
+            raise Exception(exc)
+        
+def library_ss_MIMO(system: int) -> tuple[ct.StateSpace, str]:
+    """Returns the a tuple containing the state space representation and the
+    name of the system."""
+    with open(system_library, "r") as f:
+        try:
+            systems = yaml.safe_load(f)["State Space MIMO"]
+            if system > len(systems):
+                raise ValueError(f"System {system} not found in systems.yaml")
+            ss = ct.StateSpace(systems[system]["A"], systems[system]["B"],
+                               systems[system]["C"], systems[system]["D"])
+            return ss, systems[system]["name"]
+
+        except yaml.YAMLError as exc:
+            raise Exception(exc)       
